2479585109e6e1cc01d1f6e9ca9b723ed16d85bd
Dmitriy Kuzmin <crazyrider@ukr.net>
2021-03-10 20:11:05 +0200

    basic layout and styles have been done

---
+++ src/components/Cell.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cell.css';
+
+function Cell() {
+	return (
+		<button className="cell">
+			x
+		</button>
+	)
+}
+
+export default Cell;
---
+++ src/components/Game.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './Board';
+import './Game.css';
+
+function Game() {
+	return (
+		<div className="wrapper">
+			<Board/>
+		</div>
+	)
+}
+
+export default Game;
