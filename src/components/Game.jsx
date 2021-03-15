56f57c927bdb595f34ac5f182970a965a9e5d23c
Dmitriy Kuzmin <crazyrider@ukr.net>
2021-03-16 01:35:14 +0200

    history function added

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,42 +4,59 @@ import './Game.css';
 import { calculateWinner } from '../helper';
 
 function Game() {
-	const [board, setBoard] = useState(Array(9).fill(null));
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const [stepNumber, setStepNumber] = useState(0);
 	const [xIsNext, setXIsNext] = useState(true);
-	const winner = calculateWinner(board);
+	const winner = calculateWinner(history[stepNumber]);
+	const x0 = xIsNext ? 'X' : 'O';
 
 	function handleClick(index) {
-		const boardCopy = [...board];
+		const historyPoint = history.slice(0, stepNumber + 1);
+		const current = historyPoint[stepNumber];
+		const cells = [...current];
 
-		if (winner || boardCopy[index]) return;
-		boardCopy[index] = xIsNext ? 'X' : 'O';
-		setBoard(boardCopy);
+		if (winner || cells[index]) return;
+		
+		cells[index] = x0;
+		setHistory([...historyPoint, cells]);
+		setStepNumber(historyPoint.length);
 		setXIsNext(!xIsNext);
-	}
+		
+	};
 
-	function startNewGame() {
-		return (
-			<button
-				className="start-btn"
-				onClick={() => setBoard(Array(9).fill(null))}
-			>
-				Tap to clear the playing board
-			</button>
-		)
-	}
+	const jumpTo = (step) => {
+		setStepNumber(step);
+		setXIsNext(step % 2 === 0);
+	};
 
+	const renderMoves = () =>
+		history.map((_step, move) => {
+			const destination = move ? `Go to move #${move}` : `Clear the board and GO to start`;
+
+			return (
+				<li
+					className="history-button"
+					key={move}>
+					<button onClick={() => jumpTo(move)}>{destination}</button>
+				</li>
+			);
+		});
+	
 	return (
 		<div className="wrapper">
-			{startNewGame()}
 			<Board
-				cells={board}
+				cells={history[stepNumber]}
 				click={handleClick}
 			/>
 			<p className="game-info">
-				{winner ? 'Winner is: ' + winner : 'next turn for: ' + (xIsNext ? 'X' : 'O')}
+				{winner ? 'Winner is: ' + winner : 'next turn for: ' + x0}
 			</p>
+			<div className="history-wrapper">
+				<h2>History</h2>
+				{renderMoves()}
+		</div>
 		</div>
 	)
-}
+};
 
 export default Game;
